652ba12b88fa9f8699b050a97bb6da8e0b1761c0
llamicron <llamicron@gmail.com>
2020-05-29 01:35:51 -0500

    Moves github_wiki tests into the same module

--- examples/github_wiki.rs
+++ examples/github_wiki.rs
@@ -1,9 +1,57 @@
 extern crate lab_grader;
 
 use lab_grader::*;
+// Bring all the test functions into scope
+use tests::*;
 
-mod github_tests;
-use github_tests::*;
+// The example in the guide uses an external module
+// this keeps it in the same file so it will work as an example
+// mod tests;
+
+mod tests {
+    use std::process::Command;
+    use lab_grader::*;
+
+    // Naming the data parameter "_" because we don't need it in this case
+    pub fn confirm_git_installed(_: &TestData) -> bool {
+        cli::Program::Git.version().is_some()
+    }
+
+    pub fn confirm_git_init(_: &TestData) -> bool {
+        // This is a filesystem helper that this crate provides
+        // also works on directories
+        // This is *not* std::fs
+        fs::file_exists(".git/")
+    }
+
+    pub fn confirm_enough_commits(_: &TestData) -> bool {
+        // Run the git command to list commit count
+        let out = Command::new("sh")
+            .arg("-c")
+            .arg("git rev-list --all --count")
+            .output()
+            .expect("Couldn't run subcommand");
+
+        // If the command returns something
+        if let Ok(string) = String::from_utf8(out.stdout) {
+            // And if we could parse a number from it
+            if let Ok(num) = string.trim().parse::<u64>() {
+                return num > 2;
+            }
+        }
+
+        false
+    }
+
+    // We do need the data this time
+    pub fn confirm_repo_pushed(data: &TestData) -> bool {
+        // Format the url to check
+        let url = format!("https://github.com/{}/{}/", data["gh_name"], data["repo"]);
+        // Another helper function
+        web::site_responds(&url)
+    }
+
+}
 
 
 fn main() {
@@ -34,10 +82,9 @@ fn main() {
     sub.grade_against(&mut batch);
     println!("{}", batch);
 
-    // let url = "http://myurl.whatever/submit";
-    // if web::post_json(url, &sub).is_ok() {
-    //     println!("Submission sent and recieved!");
-    // } else {
-    //     println!("Submission could not be sent.");
-    // }
+    let url = "https://postman-echo.com/post";
+    match web::post_json(url, &sub) {
+        Ok(resp) => println!("Submission sent and recieved! Status {}", resp.status()),
+        Err(_) => println!("Submission could not be sent.")
+    }
 }
