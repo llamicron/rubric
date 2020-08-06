40c7742ce34fe19970e1b00075d9ce891d3cd4a8
llamicron <llamicron@gmail.com>
2020-08-05 23:28:00 -0500

    Removes old main.rs

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use rubric::{Rubric, yaml};
-
-fn main() {
-    let yaml = yaml!("../test_data/test_rubric.yml").unwrap();
-    let rubric = Rubric::from_yaml(&yaml).unwrap();
-
-    println!("{:#?}", rubric.deadline);
-}
